2543c52cb60d0efa2a8fb93c8f666fc7ffb44d44
arabianlover <slavon1998x359x@gmail.com>
2024-04-08 15:19:35 +0500

    06.04 - 08.04 edits

--- InterfaceOfCalculator.java
+++ InterfaceOfCalculator.java
@@ -1,22 +1,15 @@
 package com.financeCalculator;
 
 import javafx.application.Application;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
-
-import javax.swing.*;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
@@ -26,217 +19,139 @@ public class InterfaceOfCalculator extends Application {
 
     public Group root = new Group();
     public VBox strings = new VBox();//VBox для всех блоков
-    final private int WIDTH = 1000;
-    final private int HEIGHT = 600;
-
     public VBox stringsAddBlock = new VBox();//VBox для блока ввода новых данных
     public VBox stringsSearchBlock = new VBox();//VBox для блока поиска данных
     public VBox stringsOutBlock = new VBox();//VBox для блока вывода данных
-
-    ComboBox<Purchase> datePurchasesComboBox = new ComboBox<>();//выпадающий список со всеми покупками за все даты
+    //ComboBox<Purchase> datePurchasesComboBox = new ComboBox<>();//выпадающий список со всеми покупками за все даты
 
 
     public void createAddBlock() {
         //здесь создается блок ввода даты, а также список имен-цен продуктов на эту дату
 
         Button buttonAddPurchase = new Button("Add purchase");    //кнопка добавления новой покупки
-        TextField addDay = new TextField();                          //поле для добавления дня
-        TextField addMonth = new TextField();                        //поле для добавления месяца
-        TextField addYear = new TextField();                         //поле для добавления года
         TextField addName = new TextField();                         //поле для добавления имени продукта
         TextField addPrice = new TextField();                        //поле для добавления цены
+        DatePicker datePicker = new DatePicker();
+        datePicker.setValue(LocalDate.now());
 
-        ObservableList<String> allTypes = FXCollections.observableArrayList
-                ("Food", "Clothes", "Travels", "Sweets", "Alcohol", "House service", "Medicine", "For friends", "Hygiene",
-                        "Connection", "Others");
-        ComboBox <String> allTypesComboBox = new ComboBox(allTypes);                 //список для выбора типа покупки
+        ComboBox <String> allTypesComboBox = new ComboBox();
+        allTypesComboBox.getItems().addAll("Food", "Clothes", "Travels", "Sweets", "Alcohol",
+                "House service", "Medicine", "For friends", "Hygiene", "Connection", "Others");//список для выбора типа покупки
+        allTypesComboBox.setValue("Food");
 
-
-
-
-        stringsAddBlock.getChildren().add(new Text("Enter the date"));
         HBox enterDateHBox = new HBox();
-        enterDateHBox.getChildren().add(new Text("Day: "));
-        enterDateHBox.getChildren().add(addDay);
-        enterDateHBox.getChildren().add(new Text("Month: "));
-        enterDateHBox.getChildren().add(addMonth);
-        enterDateHBox.getChildren().add(new Text("Year: "));
-        enterDateHBox.getChildren().add(addYear);
         stringsAddBlock.getChildren().add(enterDateHBox);
-
 
         stringsAddBlock.getChildren().add(new Text("Enter the new purchase"));
         HBox enterNameAndPriceHBox = new HBox();
-        enterNameAndPriceHBox.getChildren().add(new Text("Name "));
+        enterNameAndPriceHBox.getChildren().add(new Text("Name: "));
         enterNameAndPriceHBox.getChildren().add(addName);
-        enterNameAndPriceHBox.getChildren().add(new Text("Price "));
+        enterNameAndPriceHBox.getChildren().add(new Text("Price: "));
         enterNameAndPriceHBox.getChildren().add(addPrice);
+        enterNameAndPriceHBox.getChildren().add(new Text("Date: "));
+        enterNameAndPriceHBox.getChildren().add(datePicker);
         enterNameAndPriceHBox.getChildren().add(allTypesComboBox);
         enterNameAndPriceHBox.getChildren().add(buttonAddPurchase);
         stringsAddBlock.getChildren().add(enterNameAndPriceHBox);
 
-
         buttonAddPurchase.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
-                Purchase purchase = new Purchase(Short.parseShort(addDay.getText()), Short.parseShort(addMonth.getText()),
-                        Short.parseShort(addYear.getText()), addName.getText(), Integer.parseInt(addPrice.getText()),
+
+                LocalDate date = datePicker.getValue();
+                Purchase purchase = new Purchase(date, addName.getText(), Integer.parseInt(addPrice.getText()),
                         (String)allTypesComboBox.getSelectionModel().getSelectedItem());
+
                 listOfPurchases.add(purchase);
-                datePurchasesComboBox.getItems().addAll(purchase);
+                //datePurchasesComboBox.getItems().addAll(purchase);
                 addName.clear();
                 addPrice.clear();
-
-
             }
         });
-
     }
 
     public void createSearchBlock() {
         //здесь создается блок поиска по типу покупки и по имени покупки в указанном диапазоне дат
 
-        Button buttonSearchPurchaseType = new Button("Search purchase type");       //кнопка поиска типа покупки
-        Button buttonSearchPurchaseName = new Button("Search purchase name");        //кнопка поиска наименования покупки
-        TextField fromDay = new TextField();//поля ввода начальной даты
-        TextField fromMonth = new TextField();
-        TextField fromYear = new TextField();
-        TextField toDay = new TextField();//поля ввода конечной даты
-        TextField toMonth = new TextField();
-        TextField toYear = new TextField();
-
-
+        ComboBox comboBoxTypes = new ComboBox();
+        DatePicker fromDatePicker = new DatePicker();
+        DatePicker toDatePicker = new DatePicker();
+        fromDatePicker.setValue(LocalDate.now());
+        toDatePicker.setValue(LocalDate.now());
 
         Button buttonGetDateInfo = new Button("Ok");
-        stringsSearchBlock.getChildren().add(new Text("Search in a date range"));
+        stringsSearchBlock.getChildren().add(new Text("Search "));
         HBox dateRangeHBox = new HBox();
         dateRangeHBox.getChildren().add(new Text("from:"));
-        dateRangeHBox.getChildren().add(new Text("day: "));
-        dateRangeHBox.getChildren().add(fromDay);
-        dateRangeHBox.getChildren().add(new Text("month: "));
-        dateRangeHBox.getChildren().add(fromMonth);
-        dateRangeHBox.getChildren().add(new Text("year: "));
-        dateRangeHBox.getChildren().add(fromYear);
-
+        dateRangeHBox.getChildren().add(fromDatePicker);
         dateRangeHBox.getChildren().add(new Text("to:"));
-        dateRangeHBox.getChildren().add(new Text("day: "));
-        dateRangeHBox.getChildren().add(toDay);
-        dateRangeHBox.getChildren().add(new Text("month: "));
-        dateRangeHBox.getChildren().add(toMonth);
-        dateRangeHBox.getChildren().add(new Text("year: "));
-        dateRangeHBox.getChildren().add(toYear);
+        dateRangeHBox.getChildren().add(toDatePicker);
+
+        comboBoxTypes.getItems().addAll("All", "Food", "Clothes", "Travels", "Sweets", "Alcohol",
+                "House service", "Medicine", "For friends", "Hygiene", "Connection", "Others");
+        comboBoxTypes.setValue("All");
+
+        dateRangeHBox.getChildren().add(comboBoxTypes);
         dateRangeHBox.getChildren().add(buttonGetDateInfo);
         stringsSearchBlock.getChildren().add(dateRangeHBox);
 
-
-
         VBox searchType = new VBox();
         HBox searchInfo = new HBox();
-        Button buttonGetInfo = new Button("Info");
         Text textInfo = new Text();
-        searchType.getChildren().add(new Text("Enter the type of purchase:"));
-        searchInfo.getChildren().add(datePurchasesComboBox);
-        searchInfo.getChildren().add(buttonGetInfo);
+
         searchInfo.getChildren().add(textInfo);
         searchType.getChildren().add(searchInfo);
         stringsSearchBlock.getChildren().add(searchType);
-        
-        
-
-        buttonGetInfo.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent actionEvent) {
-                Purchase purchase = (Purchase) datePurchasesComboBox.getSelectionModel().getSelectedItem();
-                if (purchase != null){
-                    textInfo.setText("Type is " + purchase.getType() + " " + "Prise is " + purchase.getPrice() + " " +
-                            "Date is " + purchase.getDay() + ":" + purchase.getMonth() + ":" + purchase.getYear());
-                } else {
-                    textInfo.setText("Purchase is not selected");
-                }
-            }
-        });
-
-        //здесь создается блок вывода данных
-
 
         Text dateInfo = new Text();
         TextArea allPurchasesInPeriod = new TextArea();
-        ArrayList<String> stringArrayList = new ArrayList<>();
-        Text sum = new Text();
+        ArrayList<String> allPurchasesList = new ArrayList<>();
+        Text sumOut = new Text();
 
         stringsOutBlock.getChildren().add(dateInfo);
         stringsOutBlock.getChildren().add(allPurchasesInPeriod);
-        stringsOutBlock.getChildren().add(sum);
-
-
-
-
-
+        stringsOutBlock.getChildren().add(sumOut);
 
         buttonGetDateInfo.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent actionEvent) {
+                refresh(allPurchasesList);
 
+                LocalDate fromDate = fromDatePicker.getValue();//переменные для указания диапазона дат в поиске
+                LocalDate toDate = toDatePicker.getValue();
 
-                LocalDate fromDate = LocalDate.now();//переменные для указания диапазона дат в поиске
-                LocalDate toDate = LocalDate.now();
+                if(!fromDate.equals(null) && !toDate.equals(null)){
 
-                if ((!fromDay.getText().isEmpty()) && (!fromMonth.getText().isEmpty()) && (!fromYear.getText().isEmpty())
-                        && (!toDay.getText().isEmpty()) && (!toMonth.getText().isEmpty()) && (!toYear.getText().isEmpty())){
-
-                    fromDate = fromDate.plusYears(Integer.parseInt(fromYear.getText()) - fromDate.getYear());
-                    fromDate = fromDate.plusMonths(Integer.parseInt(fromMonth.getText()) - fromDate.getMonthValue());
-                    fromDate = fromDate.plusDays(Integer.parseInt(fromDay.getText()) - fromDate.getDayOfMonth());
-
-                    toDate = toDate.plusYears(Integer.parseInt(toYear.getText()) - toDate.getYear());
-                    toDate = toDate.plusMonths(Integer.parseInt(toMonth.getText()) - toDate.getMonthValue());
-                    toDate = toDate.plusDays(Integer.parseInt(toDay.getText()) - toDate.getDayOfMonth());
-
-
-
-
-                    dateInfo.setText("Your purchases from " + fromDate.getDayOfMonth() + ":" + fromDate.getMonth()
-                            + ":" + fromDate.getYear() + " to " + toDate.getDayOfMonth() + ":" + toDate.getMonth()
-                            + ":" + toDate.getYear());
-
-
+                    dateInfo.setText("Your purchases from " + fromDatePicker.getValue() + " to " + toDatePicker.getValue());
                 } else {
                     dateInfo.setText("Enter the dates, please");
                 }
-                double summa = 0;
+
+                double sum = 0;
                 for(Purchase purchase : listOfPurchases){
                     if(purchase.getDate().isAfter(fromDate) && purchase.getDate().isBefore(toDate)){
-                        stringArrayList.add(purchase.getName() + " - " + purchase.getPrice());
-                        summa = summa + purchase.getPrice();
 
-                        sum.setText("Sum is: " + summa);
+                        if(comboBoxTypes.getValue().equals("All")){
+                            allPurchasesList.add(purchase.getName() + " - " + purchase.getPrice());
+                            sum = sum + purchase.getPrice();
+                            sumOut.setText("Sum is: " + sum);
+                        } else if(comboBoxTypes.getValue().equals(purchase.getType())){
+                            allPurchasesList.add(purchase.getName() + " - " + purchase.getPrice());
+                            sum = sum + purchase.getPrice();
+                            sumOut.setText("Sum is: " + sum);
+                        }
                     }
                 }
-
-
                 String separator = "\n";
-                String res = String.join(separator, stringArrayList);
-
+                String res = String.join(separator, allPurchasesList);
                 allPurchasesInPeriod.setText(res);
-
-
-
-
-
-
-
             }
         });
-
-
-
-
-
-
-
     }
 
-
+    public static void refresh(ArrayList<String> stringArrayList){
+        stringArrayList.clear();
+    }
 
     @Override
     public void start(Stage stage) throws Exception {
@@ -246,15 +161,10 @@ public class InterfaceOfCalculator extends Application {
         strings.getChildren().add(stringsSearchBlock);
         strings.getChildren().add(stringsOutBlock);
 
-
-
         root.getChildren().add(strings);
-        Scene scene = new Scene(root, 1200, 600);
+        Scene scene = new Scene(root, 800, 400);
         stage.setTitle("FinanceCalculator");
         stage.setScene(scene);
         stage.show();
-
     }
-
-
 }
--- Purchase.java
+++ Purchase.java
@@ -1,13 +1,10 @@
 package com.financeCalculator;
 
 import java.time.LocalDate;
-import java.time.Month;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Purchase {
 
-    //Этот класс содержит поля: название, цена покупки, сумма цен. Список всех имен покупок
+    //Этот класс содержит поля: дату, название, цену и тип покупки
 
 
     private LocalDate date;
@@ -15,46 +12,35 @@ public class Purchase {
     private double price;
     private String type;
 
-    private static double sumOfPrices;
-
-    private static ArrayList<String> listOfNames = new ArrayList<>();
 
     public Purchase(short day, short month, short year, String name, double price, String type){
         this.date = LocalDate.of(year, month, day);
         this.name = name;
         this.price = price < 0 ? 0 : price;
-        listOfNames.add(name);
+        this.type = type;
+    }
+    public Purchase(LocalDate date, String name, double price, String type){
+        this.date = date;
+        this.name = name;
+        this.price = price < 0 ? 0 : price;
+        this.type = type;
     }
 
 
     public String toString(){
         return name;
     }
-
     public String getName(){
         return name;
     }
-
     public void setName(String name){
         this.name = name;
     }
-
     public double getPrice(){
         return price;
     }
-
     public void setPrice(double price){
         this.price = price < 0 ? 0 : price;
-    }
-
-    public Short getDay(){
-        return (short) date.getDayOfMonth();
-    }
-    public Short getMonth(){
-        return (short) date.getMonthValue();
-    }
-    public Short getYear(){
-        return (short) date.getYear();
     }
     public LocalDate getDate(){
         return date;
@@ -62,68 +48,5 @@ public class Purchase {
     public String getType(){
         return type;
     }
-
-
-    /*public static ArrayList<Purchase> autoCreateArrayListOfPurchases(){
-        ArrayList<Purchase> listOfPurchases = new ArrayList<>();
-        Purchase purchase = new Purchase("water", 300);
-        Purchase purchase1 = new Purchase("bread", 100);
-        Purchase purchase2 = new Purchase("honey", 2000);
-        Purchase purchase3 = new Purchase("milk", 500);
-        Purchase purchase4 = new Purchase("chicken", 1500);
-        listOfPurchases.add(purchase);
-        listOfPurchases.add(purchase1);
-        listOfPurchases.add(purchase2);
-        listOfPurchases.add(purchase3);
-        listOfPurchases.add(purchase4);
-
-        return listOfPurchases;
-    }*/
-
-    /*public static ArrayList<Purchase> createArrayListOfPurchases(){
-
-        //Этот метод создает список объектов Purchase
-        Scanner scanner = new Scanner(System.in);
-        Scanner scanner1 = new Scanner(System.in);
-
-        ArrayList<Purchase> listOfPurchases = new ArrayList<>();
-
-        while (true) {
-            String name;
-            System.out.println("Enter the name of purchase:");
-            name = scanner.nextLine();
-            if (name.equals("out"))
-                break;
-            System.out.println("Enter the price of purchase:");
-            listOfPurchases.add(new Purchase(name, scanner1.nextDouble()));
-        }
-
-        return listOfPurchases;
-    }*/
-
-    public static void printListOfPurchases(ArrayList<Purchase> listOfPurchases){
-
-        //Этот метод выводит список объектов Purchase на экран
-        for (Purchase purchase : listOfPurchases) {
-            System.out.println(purchase.getName() + "\t " + purchase.getPrice());
-        }
-    }
-
-    public static double calculateSumOfPrices(ArrayList<Purchase> listOfPurchases){
-
-        //Этот метод считает сумму аргументов price в списке объектов Purchase
-        for(Purchase purchase : listOfPurchases){
-            sumOfPrices += purchase.getPrice();
-        }
-        System.out.println("Sum of purchases is " + sumOfPrices);
-        return sumOfPrices;
-    }
-
-    public static void printNames(){
-        for (int i = 0; i < listOfNames.size(); i++) {
-            System.out.println(listOfNames.get(i));
-        }
-    }
-
 
 }
